32f023df21290b95fe8d584a5074954e865a672e
NguyenHoangAnh411 <131466310+NguyenHoangAnh411@users.noreply.github.com>
2024-11-28 21:31:23 +0700

    Update Sign Up Email

--- lib/Mobile_Screen/signup_email_bottomsheet.dart
+++ lib/Mobile_Screen/signup_email_bottomsheet.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/reuseable_items.dart';
-
+import 'package:my_project/Services/authService.dart';
+import 'package:my_project/Mobile_Screen/mobile_onboarding.dart';
 void emailSignUpScreen(BuildContext context) {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
   showModalBottomSheet(
     context: context,
     isScrollControlled: true,
     backgroundColor: Colors.white,
     shape: const RoundedRectangleBorder(
       borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(20),
-        topRight: Radius.circular(20),
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
       ),
     ),
     builder: (BuildContext context) {
@@ -20,59 +26,127 @@ void emailSignUpScreen(BuildContext context) {
           padding: const EdgeInsets.all(14.0),
           child: Column(
             children: <Widget>[
-              const SizedBox(height: 20),
+              const SizedBox(height: 10),
               const Text(
                 'Đăng ký bằng email',
                 style: TextStyle(
-                  fontSize: 25,
+                  fontSize: 22,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
                 ),
               ),
-              const SizedBox(height: 30),
-              TextField(
-                decoration: customTextFieldDecoration.copyWith(
-                  hintText: 'Tên',
-                ),
-              ),
               const SizedBox(height: 20),
-              TextField(
-                decoration: customTextFieldDecoration.copyWith(
-                  hintText: 'Nhập email của bạn',
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: customTextFieldDecoration.copyWith(
-                  hintText: 'Nhập mật khẩu',
-                ),
-                obscureText: true, // Ẩn mật khẩu khi nhập
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: customTextFieldDecoration.copyWith(
-                  hintText: 'Xác nhận mật khẩu',
-                ),
-                obscureText: true, // Ẩn mật khẩu khi nhập
-              ),
-              const SizedBox(height: 30),
               SizedBox(
-                width: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: TextField(
+                  controller: nameController,
+                  decoration: customTextFieldDecoration.copyWith(
+                    hintText: 'Tên',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: TextField(
+                  controller: emailController,
+                  decoration: customTextFieldDecoration.copyWith(
+                    hintText: 'Nhập email của bạn',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: TextField(
+                  controller: passwordController,
+                  decoration: customTextFieldDecoration.copyWith(
+                    hintText: 'Nhập mật khẩu',
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: TextField(
+                  controller: confirmPasswordController,
+                  decoration: customTextFieldDecoration.copyWith(
+                    hintText: 'Xác nhận mật khẩu',
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 15),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    String name = nameController.text.trim();
+                    String email = emailController.text.trim();
+                    String password = passwordController.text.trim();
+                    String confirmPassword = confirmPasswordController.text.trim();
+
+                    if (name.isEmpty || email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+                      _showDialog(context, 'Vui lòng điền đầy đủ thông tin!');
+                      return;
+                    }
+
+                    if (password != confirmPassword) {
+                      _showDialog(context, 'Mật khẩu và xác nhận mật khẩu không khớp!');
+                      return;
+                    }
+
+                    AuthService.signUpWithEmail(
+                      context: context,
+                      name: name,
+                      email: email,
+                      password: password,
+                      confirmPassword: confirmPassword,
+                    ).then((_) {
+                      Navigator.pop(context);
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MobileOnboarding()),
+                      );
+                    }).catchError((error) {
+                      _showDialog(context, 'Đã xảy ra lỗi: ${error.toString()}');
+                    });
+                  },
                   style: customElevatedButtonStyle,
                   child: const Text(
                     'Đăng ký',
                     style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFFFFFFF)),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFFFFFF),
+                    ),
                   ),
                 ),
               ),
             ],
           ),
         ),
+      );
+    },
+  );
+}
+
+void _showDialog(BuildContext context, String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Thông báo'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Đóng'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
       );
     },
   );
